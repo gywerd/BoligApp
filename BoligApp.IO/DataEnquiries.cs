862ed0a8d72dbdd9934168bbed6f0ef44c75261c
Baron Daniel Bernhard Rugbjerg Gammelgaard Giversen <bern0145@edu.campusvejle.dk>
2017-11-13 13:46:45 +0100

    Brainstorm navneord

---
+++ BoligApp.IO/DataEnquiries.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoligApp.IO
+{
+    public class DataEnquiries
+    {
+        #region Fields
+        protected QueryExecutor executor;
+        #endregion
+
+        #region Constructors
+        public DataEnquiries()
+        {
+            executor = new QueryExecutor();
+        }
+        #endregion
+    }
+}
